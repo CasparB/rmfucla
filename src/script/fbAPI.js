6aa50b84d6ba4893a92dfac39ca4ae119102e55f
CasparB <casparbroekhuizen@gmail.com>
2022-11-06 16:29:32 -0800

    Firebase API edits

---
+++ src/script/fbAPI.js
@@ -0,0 +1,17 @@
+// Methods
+
+// Add food to item to Foods database
+// - if item exists, do not add food and return false
+export const addFood = async (food) => {
+    
+}
+
+// Add review to food in the Foods database
+// - if food does not exist, return false
+// - if user review already exist today, return false
+// Add short form review to the Reviews database
+export const addReview = async (review, food) => {
+    
+}
+
+// 
