6bbd58972b1cc40f0d88744ba5a5cdd93417ff63
Michael R <mr2004@g.ucla.edu>
2022-11-07 22:38:13 -0800

    Webscrape the full menu and for each meal at the three cafeterias

--- src/script/webscrapeAPI.js
+++ src/script/webscrapeAPI.js
@@ -8,7 +8,8 @@ const writeStream = fs.createWriteStream('post.csv');
 writeStream.write('Hall, Meals');
 
 //urls to parse from. 
-const uclamenuURL = 'https://menu.dining.ucla.edu/Menus';
+const uclamenusURLS = ['https://menu.dining.ucla.edu/Menus/Breakfast','https://menu.dining.ucla.edu/Menus/Lunch','https://menu.dining.ucla.edu/Menus/Dinner'];
+const meals = ["Breakfast", "Lunch", "Dinner"];
 
 //global data structures / variables. 
 const API_KEY = '35L-american-aussies';
@@ -16,34 +17,37 @@ const render = true;
 let menuItems = [];
 
 //parsing in the HTML website. 
-const test = axios(uclamenuURL, {params: {
-  'url': uclamenuURL,
-  'api_key': API_KEY,
-  'render': render
-}})
-  .then(response => {
-    let foods = [];
-    const html = response.data; 
-    const $ = cheerio.load(html)
-    $('.menu-block').each((i, elem) => {
-    const hall = $(elem).text();  
-    const hallfood = $(elem).find('.recipelink').each((i, el) => {
-      let hallfood = {
-        name: $(el).text(),
-        location: $(elem).find('.col-header').text(),
-        type: 
-      }
+for(let index = 0; index < 3; index++){
+  const test = axios(uclamenusURLS[index], {params: {
+    'url': uclamenusURLS[index],
+    'api_key': API_KEY,
+    'render': render
+  }})
+    .then(response => {
+      let foods = [];
+      const html = response.data; 
+      const $ = cheerio.load(html)
+      $('.menu-block').each((i, elem) => {
+      const hall = $(elem).text();  
+      const hallfood = $(elem).find('.recipelink').each((i, el) => {
+        let hallfood = {
+          name: $(el).text(),
+          location: $(elem).find('.col-header').text(),
+          tpye: meals[index]
+        }
 
-      foods.push(hallfood)
-    });
-    });
-    
-return foods;
+        foods.push(hallfood)
+      });
+      });
+      
+  return foods;
 
-  }).catch(console.error);
+    }).catch(console.error);
 
-const logResponse = async () => {
-  console.log( await test );
+  const logResponse = async () => {
+    console.log( await test );
+  }
+
+  console.log(logResponse());
+
 }
-
-console.log(logResponse());
