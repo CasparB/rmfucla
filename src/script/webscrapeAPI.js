6cba84630d442f5a58ee537503d65d350e438d3f
Michael R <mr2004@g.ucla.edu>
2022-11-07 23:31:39 -0800

    Have webscrapper include breakfast, lunch, and dinner for the three main dining halls. It also adds the date.

--- src/script/webscrapeAPI.js
+++ src/script/webscrapeAPI.js
@@ -8,8 +8,8 @@ const writeStream = fs.createWriteStream('post.csv');
 writeStream.write('Hall, Meals');
 
 //urls to parse from. 
-const uclamenusURLS = ['https://menu.dining.ucla.edu/Menus/Breakfast','https://menu.dining.ucla.edu/Menus/Lunch','https://menu.dining.ucla.edu/Menus/Dinner'];
-const meals = ["Breakfast", "Lunch", "Dinner"];
+const uclamenusURLS = ['https://menu.dining.ucla.edu/Menus/DeNeve','https://menu.dining.ucla.edu/Menus/BruinPlate','https://menu.dining.ucla.edu/Menus/Epicuria'];
+const menuOrder = ['DeNeve', 'Bruin Plate', 'Epicuria']
 
 //global data structures / variables. 
 const API_KEY = '35L-american-aussies';
@@ -17,30 +17,36 @@ const render = true;
 let menuItems = [];
 
 //parsing in the HTML website. 
-for(let index = 0; index < 3; index++){
+for(let index = 0; index < uclamenusURLS.length; index++){
   const test = axios(uclamenusURLS[index], {params: {
     'url': uclamenusURLS[index],
     'api_key': API_KEY,
     'render': render
   }})
     .then(response => {
+      let totalInfo = [];
+      let data = {};
       let foods = [];
       const html = response.data; 
       const $ = cheerio.load(html)
+      const locationInfo = $('#page-header').text();
+      const info = locationInfo.split(',');
+      const name = (info[0]).split('Menu');
       $('.menu-block').each((i, elem) => {
-      const hall = $(elem).text();  
-      const hallfood = $(elem).find('.recipelink').each((i, el) => {
-        let hallfood = {
-          name: $(el).text(),
-          location: $(elem).find('.col-header').text(),
-          tpye: meals[index]
-        }
-
-        foods.push(hallfood)
-      });
+        foods = [];
+        const hallfood = $(elem).find('.recipelink').each((i, el) => {
+          foods.push( $(el).text());
+        });
+        data = {
+          diningHallLocation: name[0],
+          date: info[1]+info[2],
+          mealPeriod: $(elem).find('.col-header').text(),
+          foodOptions: foods,
+        };
+        totalInfo.push(data);
       });
       
-  return foods;
+  return totalInfo;
 
     }).catch(console.error);
 
